a757655361b34295f4f27fe52f6a010caa4f8fb7
John <john@future-apps.net>
2015-12-11 19:34:05 -0500

    sdf

--- driver-futurebit.c
+++ driver-futurebit.c
@@ -453,15 +453,15 @@ void futurebit_submit_nonce(struct thr_info * const thr, const uint8_t buf[8], s
     int elapsed_ms = ms_tdiff(&now_tv, &tvp_start);
 
 
-    uint64_t total_hashes = ((nonce - range)/9) * chips[0].active_cores;
-    uint64_t hashes_per_ms = total_hashes/elapsed_ms;
+    double total_hashes = ((nonce - range)/9.0) * chips[0].active_cores;
+    double hashes_per_ms = total_hashes/elapsed_ms;
     
     
     if(&thr->_tv_last_hashes_done_call == NULL)
         hashes_done2(thr, total_hashes, NULL);
     else{
         uint64_t hashes = hashes_per_ms * ms_tdiff(&now_tv, &thr->_tv_last_hashes_done_call);
-        applog(LOG_DEBUG, "HASHES %lld HASHES_PER_MS %lld", hashes, hashes_per_ms);
+        applog(LOG_DEBUG, "HASHES %lld HASHES_PER_MS %f", hashes, hashes_per_ms);
         hashes_done2(thr, hashes, NULL);
     }
 	
